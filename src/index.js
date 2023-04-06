202c8e481b9ef13c40fa8c9fdd9d969bb45c78f2
bertoko <egobakelvin@gmail.com>
2023-04-06 02:19:14 +0100

    bootstrap installed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import "font-awesome/css/font-awesome.min.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import {Provider} from "react-redux"
 import { PersistGate } from 'redux-persist/integration/react'
 import { store, persistor } from './components/reducer/PersistStore'
